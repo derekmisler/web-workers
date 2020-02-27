aa652f90d10537b2eaa5ad5ce9a0bd0d7dabfb3b
Derek Misler <derekmisler@gmail.com>
2020-02-27 13:32:47 -0500

    comment

--- src/utils/generateData.js
+++ src/utils/generateData.js
@@ -1,3 +1,8 @@
+// JavaScript is a single-threaded environment, meaning multiple scripts cannot run at the same time.
+// For example, a site that needs to handle UI events, process large amounts of API data, and manipulate the DOM.
+// All of that can't be simultaneous due to limitations in browsers' JavaScript runtime.
+// Script execution happens within a single thread.
+
 import faker from 'faker'
 
 export const generateData = () => {
--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,10 +1,5 @@
 // Yes, I know this is a terrible O(n^2), but that's the point, so shut up.
 
-// JavaScript is a single-threaded environment, meaning multiple scripts cannot run at the same time. 
-// For example, a site that needs to handle UI events, process large amounts of API data, and manipulate the DOM. 
-// All of that can't be simultaneous due to limitations in browsers' JavaScript runtime. 
-// Script execution happens within a single thread.
-
 export const sortListDescending = data => {
   const copy = [...data]
   for (var i = 0; i < copy.length - 1; i++) {
